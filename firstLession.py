0257507229187d4deab068923b70ec2021b461aa
shaw <gggshaw007@gmail.com>
2019-02-19 22:25:35 +0800

    first day

---
+++ firstLession.py
@@ -0,0 +1,18 @@
+import sys
+import numpy
+import scipy
+import matplotlib
+import sklearn
+# if __name__ == "__main__":
+#     print(sys.version)
+#     print(numpy.__version__)
+#     print(scipy.__version__)
+#     print(matplotlib.__version__)
+#     print(sklearn.__version__)
+
+
+def hello(name):
+    print("Hello", name, "!")
+
+
+hello("Machine Learning")
